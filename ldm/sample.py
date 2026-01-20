ec48ace1521834fcf805704fc7b04a8512af040b
BitSurgeon <alberto-alive@outlook.com>
2026-01-20 20:19:01 +0200

    auto-commit

--- ldm/sample.py
+++ ldm/sample.py
@@ -4,7 +4,7 @@ from torchvision.utils import save_image
 
 from models.autoencoder import AutoencoderKL
 from models.unet import UNetEps
-from diffusion.schedules import linear_bet_schedule, make_ddpm_constants
+from diffusion.schedules import linear_beta_schedule, make_ddpm_constants
 from diffusion.ddpm import DDPM
 from utils import ensure_dir, load_ckpt
 
@@ -17,3 +17,17 @@ def nmain(args):
     
     load_ckpt(args.ae_ckpt, ae, map_location=device)
     ae.eval()
+    
+    unet = UNetEps(in_ch=args.z, base=args.unet_base, tdim=args.tdim).to(device)
+    load_ckpt(args.ldm_ckpt, unet, map_location=device)
+    unet.eval()
+
+    betas = linear_beta_schedule(args.T, device=device)
+    consts = make_ddpm_constants(betas)
+    ddpm = DDPM(unet, consts)
+    
+    zH = args.size // 8
+    zW = args.size // 8
+    z = ddpm.sample((args.n, args.z, zH, zW), T=args.T)
+    z = z / args.latent_scale
+    x = ae.decode(z)
