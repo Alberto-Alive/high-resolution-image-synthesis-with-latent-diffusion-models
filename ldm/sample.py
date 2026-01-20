dbc1b1f092637037afcbca4c6f7bbbdd22dbaadf
BitSurgeon <alberto-alive@outlook.com>
2026-01-20 20:13:47 +0200

    auto-commit

---
+++ ldm/sample.py
@@ -0,0 +1,16 @@
+import argparse
+import torch
+from torchvision.utils import save_image
+
+from models.autoencoder import AutoencoderKL
+from models.unet import UNetEps
+from diffusion.schedules import linear_bet_schedule, make_ddpm_constants
+from diffusion.ddpm import DDPM
+from utils import ensure_dir, load_ckpt
+
+@torch.no_grad()
+def nmain(args):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    ensure_dir(args.out)
+    
+    
